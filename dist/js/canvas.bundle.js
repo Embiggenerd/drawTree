401122195f73968a32cd079dd7446621f1b3c14e
Igor Atakhanov <embiggenerd@gmail.com>
2022-08-21 14:26:01 -0500

    basic tree working

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -101,75 +101,242 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 
 
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
+var canvas = document.querySelector("canvas");
+var c = canvas.getContext("2d");
+var tree = {}; // break down of building a tree in canvas
+// 1. draw one parent node
+//    a. how to locate position children (1 vs 2 vs 3)
+// 2. create algorithm that draws children, then children's children
+
+console.log({
+  innerHeight: innerHeight,
+  innerWidth: innerWidth
+});
 canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.height = innerHeight; // c.translate(0, canvas.height);
+// c.scale(1, -1);
+
+var radius = 20;
+var color = "blue";
 var mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 };
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+var colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"]; // Event Listeners
 
-addEventListener('mousemove', function (event) {
+addEventListener("mousemove", function (event) {
   mouse.x = event.clientX;
   mouse.y = event.clientY;
 });
-addEventListener('resize', function () {
+addEventListener("resize", function () {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
   init();
 }); // Objects
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
+var Particle = /*#__PURE__*/function () {
+  function Particle(x, y, radius, color, name) {
+    _classCallCheck(this, Particle);
 
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.name = name; // this.velocity = {
+    //   x: Math.random() - .5,
+    //   y: Math.random() - .5
+    // }
   }
 
-  _createClass(Object, [{
+  _createClass(Particle, [{
     key: "draw",
     value: function draw() {
       c.beginPath();
       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
+      c.fillStyle = "white";
       c.fill();
+      c.stroke();
       c.closePath();
+      c.font = "30px Comic Sans MS";
+      c.fillStyle = "red";
+      c.textAlign = "center";
+      c.textBaseline = "middle";
+      c.fillText(this.name, this.x, this.y);
     }
   }, {
     key: "update",
-    value: function update() {
-      this.draw();
+    value: function update(particles) {
+      this.draw(); // for (let i = 0; i < particles.length; i++) {
+      //   const p = particles[i];
+      //   if (this === p){
+      //     console.log("this === p")
+      //     continue
+      //   }
+      //   if (utils.distance(this.x, this.y, p.x, p.y) - this.radius * 2 <
+      //   0) {
+      //     console.log(
+      //       'collision detected'
+      //     )
+      //   }
+      // }
+      // this.x += this.velocity.x
+      // this.y += this.velocity.y
     }
   }]);
 
-  return Object;
-}(); // Implementation
+  return Particle;
+}();
+
+var drawTree = function drawTree(node) {
+  var count = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  var isRootNode = count === 0;
+
+  if (isRootNode) {
+    // Root node has a fixed position
+    var x = innerWidth / 2;
+    var y = radius * 2;
+    node.canvasNode = new Particle(x, y, radius, color, node.name);
+    node.canvasNode.update();
+    node.children.forEach(function (child) {
+      child.parent = node;
+      drawTree(child, count + 1);
+    });
+  } else {
+    // Hard part: determine positioning for children
+    // 1. Create level 50px below
+    // 2. Divide level by # of children, center divisions
+    // 3. Each point is a child, left first
+    // How to center: divide total segments by two, start that far to left of parent's x
+    var numSublings = node.parent.children.length + 1;
+    var segmentLength = node.parent.canvasNode.radius * 3;
+    var totalLength = numSublings * segmentLength;
+    var leftMargin = node.parent.canvasNode.x - totalLength / 2;
+    var whichChild = node.parent.children.indexOf(node) + 1;
+    console.log({
+      whichChild: whichChild
+    });
+
+    var _x = leftMargin + segmentLength * whichChild;
+
+    var _y = node.parent.canvasNode.y + node.parent.canvasNode.radius * 4; // Could be based on # of children
 
 
-var objects;
+    node.canvasNode = new Particle(_x, _y, radius, color, node.name);
+    node.canvasNode.update(); // const d = utils.getIntercept(x, y, node.parent.canvasNode.x,node.parent.canvasNode.y)
+    // const m = utils.getSlope(x, y, node.parent.canvasNode.x,node.parent.canvasNode.y)
+    // console.log('distance of edge', d * m)
+
+    c.beginPath(); // Figure out how to add length
+    // const d = utils.distance(x, y, node.parent.canvasNode.x,node.parent.canvasNode.y)
+    // const d = utils.getIntercept(x, y, node.parent.canvasNode.x,node.parent.canvasNode.y)
+
+    var slope = _utils__WEBPACK_IMPORTED_MODULE_0___default.a.getSlope(_x, _y, node.parent.canvasNode.x, node.parent.canvasNode.y); // let beginpoint = utils.getbeginpointFromSlopeAndLength2(x, y, node.parent.canvasNode.x - x, node.parent.canvasNode.y - y, radius)
+
+    var beginpoint = _utils__WEBPACK_IMPORTED_MODULE_0___default.a.getEndpointFromSlopeAndLength(_x, _y, slope, radius, node.parent.canvasNode.x - _x, node.parent.canvasNode.y - _y); // Get distance from beginning points to node parents, then subtract radius to get distance of actual line
+
+    var distance = _utils__WEBPACK_IMPORTED_MODULE_0___default.a.getDistance(beginpoint.x, beginpoint.y, node.parent.canvasNode.x, node.parent.canvasNode.y) - radius;
+    var endpoint = _utils__WEBPACK_IMPORTED_MODULE_0___default.a.getEndpointFromSlopeAndLength(beginpoint.x, beginpoint.y, slope, distance, node.parent.canvasNode.x - _x, node.parent.canvasNode.y - _y);
+    console.log({
+      beginpoint2: beginpoint,
+      beginpoint1: {
+        x: _x,
+        y: _y
+      },
+      slope: slope,
+      slope2: _utils__WEBPACK_IMPORTED_MODULE_0___default.a.getSlope(_x, _y, beginpoint.x, beginpoint.y),
+      distance: radius,
+      distance2: _utils__WEBPACK_IMPORTED_MODULE_0___default.a.getDistance(_x, _y, beginpoint.x, beginpoint.y),
+      parnetName: node.parent.name,
+      name: node.name
+    });
+    c.moveTo(beginpoint.x, beginpoint.y);
+    c.lineTo(endpoint.x, endpoint.y); // c.globalCompositeOperation='source-out';
+
+    c.stroke(); // c.globalCompositeOperation='source-over';
+    // c.beginPath();
+    // c.arc(this.x, this.y, 5, 0, Math.PI * 2, false);
+    // c.fillStyle = "black";
+    // c.fill();
+    // c.stroke();
+    // c.closePath();
+
+    node.children.forEach(function (child) {
+      child.parent = node;
+      drawTree(child, count + 1);
+    });
+  }
+}; // Implementation
+
+
+var particles;
 
 function init() {
-  objects = [];
-
-  for (var i = 0; i < 400; i++) {// objects.push()
-  }
+  tree = {
+    type: "circle",
+    name: "0",
+    children: [{
+      type: "circle",
+      name: "1",
+      children: [{
+        type: "circle",
+        name: "3",
+        children: []
+      }, {
+        type: "circle",
+        name: "4",
+        children: []
+      }]
+    }, {
+      type: "circle",
+      name: "2",
+      children: []
+    }]
+  }; // particles = [];
+  // circle = new Circle(300, 300, 100, 'black')
+  // circle2 = new Circle(null, null, 30, 'red')
+  // objects = []
+  // for (let i = 0; i < 4; i++) {
+  //   const radius = 80;
+  //   const color = "blue";
+  //   let x = utils.randomIntFromRange(radius, canvas.width - radius);
+  //   let y = utils.randomIntFromRange(radius, canvas.height - radius);
+  //   if (i !== 0) {
+  //     for (let j = 0; j < particles.length; j++) {
+  //       if (
+  //         utils.distance(x, y, particles[j].x, particles[j].y) - radius * 2 <
+  //         0
+  //       ) {
+  //         x = utils.randomIntFromRange(radius, canvas.width - radius * 2);
+  //         y = utils.randomIntFromRange(radius, canvas.height - radius);
+  //         j = -1;
+  //       }
+  //     }
+  //   }
+  //   particles.push(new Particle(x, y, radius, color));
 } // Animation Loop
 
 
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  c.clearRect(0, 0, canvas.width, canvas.height); // circle.update()
+  // circle2.x = mouse.x
+  // circle2.y = mouse.y
+  // circle2.update()
+  // if(utils.getDistance(circle.x, circle.y, circle2.x, circle2.y) < circle.radius + circle2.radius) {
+  //   circle.color = 'red'
+  // } else {
+  //   circle.color = 'black'
+  // }
+  // console.log(utils.getDistance(circle.x, circle.y, circle2.x, circle2.y))
+  // c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
+  // particles.forEach((particle) => {
+  //   particle.update(particles);
+  // });
+
+  drawTree(tree);
 }
 
 init();
@@ -183,6 +350,10 @@ animate();
   \*************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
+
+function cartToScreen(px, py) {
+  return [px, -py + innerHeight];
+}
 
 function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -198,10 +369,92 @@ function distance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
+function getDistance(x1, y1, x2, y2) {
+  console.log("distance", x1, y1, x2, y2);
+  var xd = x2 - x1;
+  var yd = y2 - y1;
+  return Math.sqrt(Math.pow(xd, 2) + Math.pow(yd, 2));
+}
+
+function getSlope(x1, y1, x2, y2) {
+  // [x1, y1] = cartToScreen(x1, y1)
+  // [x2, y2] = cartToScreen(x2, y2)
+  return (y2 - y1) / (x2 - x1);
+}
+
+function getSlope2(x1, y1, x2, y2) {
+  // [x1, y1] = cartToScreen(x1, y1)
+  // [x2, y2] = cartToScreen(x2, y2)
+  return {
+    x: x2 - x1,
+    y: y2 - y1
+  };
+}
+
+function getIntercept(x1, y1, x2, y2) {
+  // y = mx + b
+  // y - mx = b
+  var m = getSlope(x1, y1, x2, y2);
+  return y1 - m * x1;
+}
+
+function getEndpointFromSlopeAndLength(x, y, m, d, xdiff, ydiff) {
+  // Math.sqrt(Math.pow(xd, 2) + Math.pow(yd, 2));
+  console.log(m * d);
+  var angle = getAngleFromSlope(xdiff, ydiff);
+  console.log({
+    angle: angle
+  }); //   new_x = old_x + cos(angle) * distance
+  // new_y = old_y + sin(angle) * distance
+
+  x1 = x + Math.cos(angle) * d;
+  y1 = y + Math.sin(angle) * d;
+  return {
+    x: x1,
+    y: y1
+  }; // return x1, y1
+}
+
+function getEndpointFromSlopeAndLength2(x, y, xdiff, ydiff, d) {
+  // Math.sqrt(Math.pow(xd, 2) + Math.pow(yd, 2));
+  var m = ydiff / xdiff;
+  console.log(m * d);
+  var angle = Math.atan2(xdiff, ydiff);
+  console.log({
+    angle: angle
+  }); //   new_x = old_x + cos(angle) * distance
+  // new_y = old_y + sin(angle) * distance
+
+  x1 = x + Math.cos(angle) * d;
+  y1 = y + Math.sin(angle) * d;
+  return {
+    x: x1,
+    y: y1
+  }; // return x1, y1
+}
+
+function getAngleFromSlope(x, y) {
+  var slope = y / x;
+  console.log("tantatnantantantantan", Math.atan(slope), Math.atan2(x, y), slope);
+
+  if (Math.atan2(x, y) < 0) {
+    return Math.atan(slope) + 3.14;
+  }
+
+  return Math.atan(slope);
+}
+
 module.exports = {
+  getEndpointFromSlopeAndLength: getEndpointFromSlopeAndLength,
+  getAngleFromSlope: getAngleFromSlope,
   randomIntFromRange: randomIntFromRange,
+  getSlope: getSlope,
   randomColor: randomColor,
-  distance: distance
+  distance: distance,
+  getDistance: getDistance,
+  getIntercept: getIntercept,
+  cartToScreen: cartToScreen,
+  getEndpointFromSlopeAndLength2: getEndpointFromSlopeAndLength2
 };
 
 /***/ })
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,16 +1,95 @@
+function cartToScreen(px, py) {
+  return [px, -py + innerHeight];
+}
+
 function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)]
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function distance(x1, y1, x2, y2) {
-  const xDist = x2 - x1
-  const yDist = y2 - y1
+  const xDist = x2 - x1;
+  const yDist = y2 - y1;
 
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
-module.exports = { randomIntFromRange, randomColor, distance }
+function getDistance(x1, y1, x2, y2) {
+  console.log("distance", x1, y1, x2, y2)
+  let xd = x2 - x1;
+  let yd = y2 - y1;
+
+  return Math.sqrt(Math.pow(xd, 2) + Math.pow(yd, 2));
+}
+
+function getSlope(x1, y1, x2, y2) {
+  // [x1, y1] = cartToScreen(x1, y1)
+  // [x2, y2] = cartToScreen(x2, y2)
+  return (y2 - y1) / (x2 - x1);
+}
+
+function getSlope2(x1, y1, x2, y2) {
+  // [x1, y1] = cartToScreen(x1, y1)
+  // [x2, y2] = cartToScreen(x2, y2)
+  return { x: x2 - x1, y: y2 - y1 };
+}
+
+function getIntercept(x1, y1, x2, y2) {
+  // y = mx + b
+  // y - mx = b
+  const m = getSlope(x1, y1, x2, y2);
+  return y1 - m * x1;
+}
+
+function getEndpointFromSlopeAndLength(x, y, m, d, xdiff, ydiff) {
+  // Math.sqrt(Math.pow(xd, 2) + Math.pow(yd, 2));
+  console.log(m * d);
+  const angle = getAngleFromSlope(xdiff, ydiff);
+  console.log({ angle });
+  //   new_x = old_x + cos(angle) * distance
+  // new_y = old_y + sin(angle) * distance
+  x1 = x + Math.cos(angle) * d;
+  y1 = y + Math.sin(angle) * d;
+  return { x: x1, y: y1 };
+  // return x1, y1
+}
+
+
+function getEndpointFromSlopeAndLength2(x, y, xdiff, ydiff, d) {
+  // Math.sqrt(Math.pow(xd, 2) + Math.pow(yd, 2));
+  const m = ydiff/xdiff
+  console.log(m * d);
+  const angle = Math.atan2(xdiff, ydiff);
+  console.log({ angle });
+  //   new_x = old_x + cos(angle) * distance
+  // new_y = old_y + sin(angle) * distance
+  x1 = x + Math.cos(angle) * d;
+  y1 = y + Math.sin(angle) * d;
+  return { x: x1, y: y1 };
+  // return x1, y1
+}
+
+function getAngleFromSlope(x, y) {
+  const slope = y/x
+  console.log("tantatnantantantantan", Math.atan(slope), Math.atan2(x, y), slope)
+  if (Math.atan2(x, y) < 0) {
+    return Math.atan(slope) + 3.14;
+  }
+  return Math.atan(slope);
+}
+
+module.exports = {
+  getEndpointFromSlopeAndLength,
+  getAngleFromSlope,
+  randomIntFromRange,
+  getSlope,
+  randomColor,
+  distance,
+  getDistance,
+  getIntercept,
+  cartToScreen,
+  getEndpointFromSlopeAndLength2
+};
